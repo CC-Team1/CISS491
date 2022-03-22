48f038c1eda662d82a19fa52f013026348f52d29
Brenton Fry <blfry1@ccis.edu>
2022-03-22 19:52:45 -0400

    Extracted Afaf's ZIP file into the project.

---
+++ Controllers/ParticipantController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADB_Fishing_Tournament.Controllers
+{
+    public class ParticipantController : Controller
+    {
+       // GET: /<controller>/
+       public string CheckRegistration()
+        {
+            return "REGISTRATION SUCCESS";
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace ADB_Fishing_Tournament.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Home()
+        {
+            ViewData["Tournament"] = "ADB Fishing Tournament";
+            ViewData["Participants"] = 50;
+            ViewData["Events"] = new List<string> { "Burbot Bash", "East Canyon Ice Fishing Derby with Fins & Fur",
+            "Slotbuster Ice Fishing Tournament"};
+            return View();
+        }
+    }
+}
